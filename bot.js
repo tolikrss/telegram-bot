f649eb38f636b957a1e79f04bbe159300b2231e3
tolikrss <tolikrss@gmail.com>
2018-09-24 17:13:06 +0300

    update coub video detection

--- bot.js
+++ bot.js
@@ -16,23 +16,34 @@ console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 bot.on('message', (msg) => {
 	const name = msg.from.first_name;
 	const kolianID = 466035983;
+	const isMsgFromKolian = msg.from.id === kolianID;
+	const isCoubVideo = msg.text && msg.text.indexOf('coub') !== -1;
 	console.dir(msg);
 	try {
 		if (
-			msg.from.id === kolianID && msg.text
+			isMsgFromKolian
+			&& msg.text
 		) {
 			const resMsg = name + ', і шо дальше?';
 			bot.sendMessage(msg.chat.id, resMsg).then(() => {
 				// reply sent!
 			});
-		} else if (
-			msg.from.id === kolianID
+		}
+		if (
+			isMsgFromKolian
 			&& !!msg.left_chat_member
 			&& msg.left_chat_member.id === kolianID
 		) {
 			const resMsg = name + ', ШО ОПЯТЬ?';
 			bot.sendMessage(msg.chat.id, resMsg);
 			// bot.getChat(msg.chat.id).
+		}
+		if (
+			isMsgFromKolian
+			&& isCoubVideo
+		) {
+			const resMsg = ', знову х*йня якась :(';
+			bot.sendMessage(msg.chat.id, resMsg);
 		}
 	} catch (e) {
 		// console.error(3);
