e6d89d115241fc2dda98cfc789377f6ce3abd424
tolikrss <tolikrss@gmail.com>
2018-09-24 10:48:48 +0300

    add bot api key

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const Telegraf = require('telegraf');
+const bot = new Telegraf('576805895:AAE5Pg2SMGXWgvup1kcRL0NK0GymOLb--zM');
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+	.use(express.static(path.join(__dirname, 'public')))
+	.set('views', path.join(__dirname, 'views'))
+	.set('view engine', 'ejs')
+	.get('/', (req, res) => res.render('pages/index'))
+	.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+bot.start((ctx) => {
+	console.log('Id пользователя:', ctx.from.id);
+	return ctx.reply('Добро пожаловать!');
+});
